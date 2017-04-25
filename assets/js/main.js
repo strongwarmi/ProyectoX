e8332a70f8c551773ccf14d8b752f1425f05555a
strongwarmi <elianasc10@hotmail.com>
2017-04-25 18:40:06 -0500

    efecto images moveover

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,21 @@ window.addEventListener('load',OcultarModal);
 
 //var img = document.getElementById('img1');
 var spanX = document.getElementsByClassName("Close")[0];
+function BordeGris(){
+  ImgFilter.style.border="1px solid #ddd";
+  img1.style.transform="rotateY(360deg)";
+};
 function OcultarModal(){
-  ModalInformativo.style.display="none";
+  Modal.style.display="none";
   ImgFilter.style.border="1px solid #ddd";
 };
 function RotateImg() {
   ImgFilter.style.border="1px solid rgba(233, 30, 99, 0.47)";
-  img1.style.rotateY="180deg";
+  img1.style.transform="rotateY(180deg)";
 }
 MaskImg1.addEventListener('click',function(){
-ModalInformativo.style.display="block";
+Modal.style.display="block";
 });
 spanX.addEventListener('click',OcultarModal);
 MaskImg1.addEventListener('mouseover',RotateImg);
+MaskImg1.addEventListener('mouseout',BordeGris);
