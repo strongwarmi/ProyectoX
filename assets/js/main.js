021bb10a81b2a75da9248dccd4f203da1bb57371
strongwarmi <elianasc10@hotmail.com>
2017-04-25 16:12:23 -0500

    imagenes modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
-var modal =  document.getElementById('modal01');
-var img = document.getElementById('img1');
+//var modal =  document.getElementById('modal01');
+
+window.addEventListener('load',OcultarModal);
+
+//var img = document.getElementById('img1');
 var spanX = document.getElementsByClassName("Close")[0];
 function OcultarModal(){
-  modal.style.display="none";
+  ModalInformativo.style.display="none";
+  ImgFilter.style.border="1px solid #ddd";
 };
-window.addEventListener('load',OcultarModal);
-img.addEventListener('click',function(){
-modal.style.display="block";
+function RotateImg() {
+  ImgFilter.style.border="1px solid rgba(233, 30, 99, 0.47)";
+  img1.style.rotateY="180deg";
+}
+MaskImg1.addEventListener('click',function(){
+ModalInformativo.style.display="block";
 });
 spanX.addEventListener('click',OcultarModal);
+MaskImg1.addEventListener('mouseover',RotateImg);
